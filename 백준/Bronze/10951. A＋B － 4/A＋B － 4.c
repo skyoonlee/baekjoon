075064154fc6b78251c29c3bf9e34b1ef23e159a
skyoon <159049930+skyoonlee@users.noreply.github.com>
2025-10-20 16:08:48 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+      int A, B ;
+      int fin;
+    
+    while(scanf("%d %d", &A, &B) != EOF) {
+ 
+    printf("%d\n", A + B);
+    
+}
+    return 0;
+
+}
+
